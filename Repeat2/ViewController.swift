e009002a74805675295f91385c0bcb40827f2895
Mila <milla209@mail.ru>
2021-04-15 20:31:33 +0300

    Label & Segment Controll

--- Repeat2/ViewController.swift
+++ Repeat2/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var segmentedControll: UISegmentedControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Label
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+        //Segmented Controll
+        segmentedControll.insertSegment(withTitle: "Third", at: 2, animated: false)
     }
 
-
+    @IBAction func segmentedControllAction() {
+        switch segmentedControll.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .black
+        case 2:
+            mainLabel.text = "The third segment is selected"
+            mainLabel.textColor = .yellow
+        default:
+            <#code#>
+        }
+    }
+    
 }
 
